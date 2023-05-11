f8b89d89595179321943e188e8ecc86cae94fc2a
msoderquist <msoderquist@mastercontrol.com>
2023-05-11 14:46:33 -0600

    Enable GH CI

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p "${HOME}/.ssh"
-gpg --quiet --batch --yes --decrypt --passphrase=${DSR_GPG_PASSWORD} --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
-gpg --quiet --batch --yes --decrypt --passphrase=${DSR_GPG_PASSWORD} --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
-gpg --quiet --batch --yes --decrypt --passphrase=${DSR_GPG_PASSWORD} --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
 chmod 600 "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa.pub"
 chmod 600 "${HOME}/.ssh/known_hosts"
