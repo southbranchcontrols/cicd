0f79487f7626a18d266540ea17cdb6106c2eb075
msoderquist <msoderquist@mastercontrol.com>
2023-09-25 16:06:30 -0600

    Update build config

--- config.sh
+++ config.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 mkdir -p "${HOME}/.ssh"
+gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output .github/avereon.keystore .github/avereon.keystore.gpg
 gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
 gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
 gpg --quiet --batch --yes --decrypt --passphrase=${SBC_GPG_PASSWORD} --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
 chmod 600 "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa.pub"
 chmod 600 "${HOME}/.ssh/known_hosts"
-ls -l "${HOME}/.ssh/id_rsa"
-ls -l "${HOME}/.ssh/id_rsa.pub"
-ls -l "${HOME}/.ssh/known_hosts"
+
+case "${GITHUB_REF}" in
+  "refs/heads/main") SBC_RELEASE="latest" ;;
+  "refs/heads/stable") SBC_RELEASE="stable" ;;
+esac
+export PRODUCT_DEPLOY_PATH=/opt/sbc/store/${SBC_RELEASE}/${SBC_PRODUCT}
+
+echo "Build date=$(date)"
+echo "GITHUB_REF=${GITHUB_REF}"
+echo "PRODUCT_DEPLOY_PATH=${PRODUCT_DEPLOY_PATH}"
+
+echo "JAVA_DISTRO=temurin" >> $GITHUB_ENV
+echo "JAVA_PACKAGE=jdk" >> $GITHUB_ENV
+echo "JAVADOC_DEPLOY_PATH=/opt/sbc/store/latest/${SBC_PRODUCT}" >> $GITHUB_ENV
+echo "JAVADOC_TARGET_PATH=/opt/sbc/web/product/${SBC_PRODUCT}/javadoc" >> $GITHUB_ENV
+echo "PRODUCT_DEPLOY_PATH=${PRODUCT_DEPLOY_PATH}" >> $GITHUB_ENV
